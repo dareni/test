5de0d8b363f31183014a8416876fbc2cec9bf481
Daren Isaacs <ikkyisaacs@gmail.com>
2014-04-07 18:26:58 +1000

    Add serialization test logic.

--- itemdirectory/itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/HomePage.java
+++ itemdirectory/itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/HomePage.java
@@ -1,11 +1,14 @@
 package pl.tomaszdziurko.itemdirectory.web.view;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import pl.tomaszdziurko.itemdirectory.service.users.SerialUtil;
 
-import pl.tomaszdziurko.itemdirectory.domain.dao.users.UserDao;
 import pl.tomaszdziurko.itemdirectory.service.users.UserService;
 import pl.tomaszdziurko.itemdirectory.web.BasePage;
 
@@ -19,10 +22,19 @@ public class HomePage extends BasePage {
 	}
 
 	private void initGui() {
-		add(new Label("helloLabel", "Wicket is saying 'Hello' to you! :)"));
+		add(new Label("helloLabel", "HomePage"));
 		add(new Label("currentTime", new Date().toString()));
-		
 		add(new Label("numberOfUsers", userService.size() +""));
 	}
+
+    @Override
+    protected void onDetach() {
+        super.onDetach();
+        try {
+            SerialUtil.doSerialTestOut(this);
+        } catch (IOException ex) {
+            Logger.getLogger(HomePage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
