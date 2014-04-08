39f65a432930416c0ce182167ac403f9794bfdeb
Daren Isaacs <ikkyisaacs@gmail.com>
2014-04-08 19:15:11 +1000

    Adjust example.

--- itemdirectory/itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/HomePage.java
+++ itemdirectory/itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/HomePage.java
@@ -4,8 +4,14 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import pl.tomaszdziurko.itemdirectory.service.users.SerialUtil;
 
@@ -22,9 +28,27 @@ public class HomePage extends BasePage {
 	}
 
 	private void initGui() {
-		add(new Label("helloLabel", "HomePage"));
+        Label label = new Label("helloLabel", "HomePage");
+        final Model<Component> swapOut = new Model();
+        swapOut.setObject(label);
+        
+        label.setOutputMarkupId(true);
+        label.setMarkupId("label");
+		add(swapOut.getObject());
 		add(new Label("currentTime", new Date().toString()));
 		add(new Label("numberOfUsers", userService.size() +""));
+        AjaxLink ajaxLink = new AjaxLink("button") {
+                                @Override
+                                public void onClick(AjaxRequestTarget target) {
+                                    Label nlabel = new Label("helloLabel", "clicked");
+                                    swapOut.getObject().replaceWith(nlabel);
+                                    swapOut.setObject(nlabel);
+                                    nlabel.setOutputMarkupId(true);
+                                    target.addComponent(swapOut.getObject());
+                                }
+                            };
+        ajaxLink.setMarkupId("button");
+        add(ajaxLink);
 	}
 
     @Override
